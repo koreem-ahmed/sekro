211d618eaeada3c8c1cce1c8da9a9c2bb352181d
kareem-ahmed <karim.1024112@stemoctober.moe.edu.eg>
2025-10-27 01:37:41 +0300

    exporting and finalizing the project and polishing it

--- scripts/build_2.gd
+++ scripts/build_2.gd
@@ -9,4 +9,4 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_file("")
+	get_tree().change_scene_to_file("res://scenes/level_1_building_2.tscn")
---
+++ scripts/game_maneger.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var score = 0
+
+func add_score():
+	score += 100
+	print("scooore++")
